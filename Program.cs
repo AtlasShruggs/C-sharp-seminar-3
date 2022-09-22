f884f9e9bb8ed7bcc631ee2ae1733794e49c02cd
Nikita <ezio13@yandex.ru>
2022-09-22 21:22:55 +0300

    добавлено решение задачи 19

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите номер задачи");
+
+int exNum = int.Parse(Console.ReadLine());
+
+if (exNum == 19)
+    {
+    Console.WriteLine("Введите число");
+    int num = Math.Abs(int.Parse(Console.ReadLine()));
+    string str = num.ToString();
+    char[] arr = str.ToCharArray();
+    Array.Reverse(arr);
+    string newStr = new string(arr); 
+    if (str == newStr)
+    {Console.WriteLine("Палиндром");}
+    }
+//else if (exNum ==)
